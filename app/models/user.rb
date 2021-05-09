e8c6e48a0e63b465ba8504a05ae14356bdc5ba89
saraya103 <saraya103@gmail.com>
2021-05-10 02:42:20 +0900

    testcode commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Input half-width alphanumeric characters.'
 
   with_options presence: true do
     validates :first_name
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Name.last_name}
+    nickname {Faker::Name.initials}
   end
 end
