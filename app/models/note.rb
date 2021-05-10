6e37425527ae6fea1ca7ec58d90281b2a9dee81d
saraya103 <saraya103@gmail.com>
2021-05-11 07:40:09 +0900

    validate&testcode setting

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,7 +6,15 @@ class Note < ApplicationRecord
   has_many :note_users, foreign_key: 'note_id'
   has_many :users, through: :note_users
 
-
+  with_options presence: true do
+    validates :id, uniqueness: { case_sensitive: false }
+    validates :password
+    validates :owner
+    validates :list_name
+    validates :log_name
+    validates :reserve_name
+    validates :item_name
+  end
   validates :genre_id, numericality: { other_than: 0 } 
 
   private
--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :note do
-    
+    id {Faker::Lorem.characters(number: 12)}
+    password {Faker::Lorem.characters(number: 6)}
+    owner {Faker::Number.number(digits: 4)}
+    genre_id {1}
+    list_name {Faker::Lorem.sentence}
+    log_name {Faker::Lorem.sentence}
+    reserve_name {Faker::Lorem.sentence}
+    item_name {Faker::Lorem.sentence}
   end
 end
