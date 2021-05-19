cfc9cb5bc3f4603369cd37b27309f1f1d39bcbe4
saraya103 <saraya103@gmail.com>
2021-05-19 11:59:46 +0900

    controller&routes setting

---
+++ app/controllers/reserves_controller.rb
@@ -0,0 +1,23 @@
+class ReservesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def reserve_params
+  end
+end
