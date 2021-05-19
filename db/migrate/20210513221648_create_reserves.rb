120cbded1de1e636bd9dbcf7834bf7282cb432f2
saraya103 <saraya103@gmail.com>
2021-05-19 11:51:58 +0900

    reserve table create

--- db/migrate/20210513221648_create_reserves.rb
+++ db/migrate/20210513221648_create_reserves.rb
@@ -1,7 +1,11 @@
 class CreateReserves < ActiveRecord::Migration[6.0]
   def change
     create_table :reserves do |t|
-
+      t.date       :date,           null: false
+      t.integer    :next_id,        null: false, default: 0
+      t.integer    :once,           null: false, default: 0
+      t.references :note,           foreign_key: true
+      t.references :item,           foreign_key: true
       t.timestamps
     end
   end
