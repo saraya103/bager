7131fdde380c5feb040bb0107ff7769c1bb53c4d
saraya103 <saraya103@gmail.com>
2021-05-25 11:14:57 +0900

    comment migration

--- db/migrate/20210525015934_create_comments.rb
+++ db/migrate/20210525015934_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.string     :content
+      t.references :note,           foreign_key: true
+      t.references :item,           foreign_key: true
       t.timestamps
     end
   end
