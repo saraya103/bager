a9447d2214e41574119105f547e02e12d4be4c3a
saraya103 <saraya103@gmail.com>
2021-05-25 11:31:02 +0900

    assosiation setting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :note
+  has_one_attached :image
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
