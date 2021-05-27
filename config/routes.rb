54736ae35c62f40443a1f26fe3267f2358d226d2
saraya103 <saraya103@gmail.com>
2021-05-27 18:46:07 +0900

    guide routes update(edit,update,destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'guides#index'
-  resources :guides, only: [:index]
+  resources :guides, only: [:index, :edit, :update, :destroy]
   resources :notes, only: [:index, :new, :create, :show], param: :character do
     resources :items
     resources :lists, only: [:index, :new, :create, :update, :destroy]
