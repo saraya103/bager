b59880a523ae6dcb3c15be8d8ec4cf8e0cfcafe7
saraya103 <saraya103@gmail.com>
2021-05-28 16:50:25 +0900

    note view update(ID&PASS show)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,7 +19,6 @@ class NotesController < ApplicationController
 
   def show
     @note = Note.find_by(character: params[:character])
-    @messages = @note.messages.order("created_at DESC")
   end
 
   private
