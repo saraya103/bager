2ff00e8d3e76c7a1cc374f6d62d2c0a4e9c3aea4
saraya103 <saraya103@gmail.com>
2021-05-28 16:07:30 +0900

    view&path setting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
-  def show
-    @note = Note.find_by(character: params[:character])
-    @messages = @note.messages.order("created_at DESC")
+  def index
+    @messages = Message.includes(:note).order("created_at DESC")
+    @note = Note.find_by(character: params[:note_character])
+    # @messages = @note.messages.order("created_at DESC")
   end
 end
