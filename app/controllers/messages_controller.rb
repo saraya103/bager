c83ba80c123428c5900d7e99af94ae9963ffd05a
saraya103 <saraya103@gmail.com>
2021-05-28 15:47:24 +0900

    controller&routes setting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,6 @@
 class MessagesController < ApplicationController
+  def show
+    @note = Note.find_by(character: params[:character])
+    @messages = @note.messages.order("created_at DESC")
+  end
 end
