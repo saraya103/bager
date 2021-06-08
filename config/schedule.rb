a70b8ef62ebe138fb9c2aa2b6b0d17f4b9d9940b
saraya103 <saraya103@gmail.com>
2021-06-08 16:03:40 +0900

    whenever setting(production)

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,9 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 set :environment, Rails.env.to_sym
 env :PATH, ENV['PATH']
-set :output, "#{Rails.root.to_s}/log/cron.log"
+set :output, environment == 'development' ? 'log/crontab.log' : '/deploy/apps/bager/shared/log/crontab.log'
 set :job_template, "/bin/bash -l -c ':job'"
 job_type :runner, "cd :path && bundle exec rails runner -e :environment ':task' :output"
+job_type :rake, 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
 
 every :day, at: "0:00 am" do
   runner "Reserve.date_check"
