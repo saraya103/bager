18d6ffae02f41b14c057de0050eed243d5ffaab0
saraya103 <saraya103@gmail.com>
2021-06-14 23:02:02 +0900

    controller update(edit&update)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   before_action :authenticate_user!
-  before_action :owner_check, only: :show
+  before_action :owner_check, only: [:show, :edit, :destroy]
 
   def index
     @notes = User.find(current_user.id).notes
@@ -23,6 +23,23 @@ class NotesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @note = Note.find(params[:character])
+    redirect_to root_path unless @note.owner == current_user.id
+    @user = User.find(params.require(:owner))
+    if @note.update(edit_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
   private
   def owner_check
     @note = Note.find_by(character: params[:character])
@@ -31,6 +48,10 @@ class NotesController < ApplicationController
 
   def note_params
     params.require(:note).permit(:title, :genre_id, :list_name, :log_name, :reserve_name, :item_name, user_ids:[]).merge(owner: current_user.id, owner_name: current_user.nickname)
+  end
+
+  def edit_params
+    params.require(:note).permit(:title, :genre_id, :list_name, :log_name, :reserve_name, :item_name).merge(owner: @user.id, owner_name: @user.nickname)
   end
 
   def set_char
