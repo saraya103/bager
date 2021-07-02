a7202bb752bc75f6eadf00b77e06faf2ddbff9aa
saraya103 <saraya103@gmail.com>
2021-07-02 14:02:48 +0900

    genre model update

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,10 @@ class Genre < ActiveHash::Base
   self.data = [
     { id: 0, name: '以下から選択' },
     { id: 1, name: '買い物' },
+    { id: 2, name: '旅行' },
+    { id: 3, name: '仕事' },
+    { id: 4, name: '趣味' },
+    { id: 5, name: 'その他' },
   ]
 
   include ActiveHash::Associations
